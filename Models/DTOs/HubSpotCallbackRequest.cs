e0516aed53477fc883b3db9fab132b5b29616916
Marc DIBEH <dibemark.job@gmail.com>
2025-10-18 12:09:19 +0200

    add hubspot dto

---
+++ Models/DTOs/HubSpotCallbackRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace FinancialAdvisorAI.API.Models.DTOs
+{
+    public class HubSpotCallbackRequest
+    {
+        public int UserId { get; set; }
+        public string Code { get; set; } = string.Empty;
+        public string State { get; set; } = string.Empty;
+    }
+}
