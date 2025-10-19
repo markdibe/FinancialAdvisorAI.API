907aa0eecf7b4fe2e03975837aa0853fb495140d
Marc DIBEH <dibemark.job@gmail.com>
2025-10-19 08:46:38 +0200

    add synclog model

---
+++ Models/SyncLog.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinancialAdvisorAI.API.Models
+{
+    public class SyncLog
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public User? User { get; set; }
+
+        [Required]
+        public string SyncType { get; set; } = string.Empty;
+
+        public DateTime StartedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? CompletedAt { get; set; }
+
+        [Required]
+        public string Status { get; set; } = "Running";
+
+        public int ItemsProcessed { get; set; }
+        public int ItemsAdded { get; set; }
+        public int ItemsUpdated { get; set; }
+
+        public string? ErrorMessage { get; set; }
+        public string? Details { get; set; }
+    }
+}
